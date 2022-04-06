9843df464afb57a82778031cddd31fd3125879bc
Sadab Halim <sadabhalim7@gmail.com>
2022-04-07 00:45:16 +0530

    Tree

--- Data Structures and Algorithms/14-Tree/code/09-Check for balanced-height binary tree/Main.java
+++ Data Structures and Algorithms/14-Tree/code/09-Check for balanced-height binary tree/Main.java
@@ -7,24 +7,27 @@ class Node {
     }
 }
 
-class Naive_Method {
-    /* ------------------ T.C: O(n) ------------------ */
+class Main {
+    /* ------------------ T.C: O(n), A.S: O(h) ------------------ */
     public static boolean isBalanced(Node root) {
-        if(root == null)
-            return true;
-        int leftSubtree = height(root.left);
-        int rightSubtree = height(root.right);
-
-        if(Math.abs(leftSubtree - rightSubtree) <= 1 && isBalanced(root.left) && isBalanced(root.right)) 
-            return true;
-        return false;
+        return dfsHeight(root) != -1;
     }
 
-    public static int height(Node root) {
+    public static int dfsHeight (Node root) {
         if(root == null)
             return 0;
-        return (1 + Math.max(height(root.left), height(root.right)));
+        int leftHeight = dfsHeight(root.left);
+        if(leftHeight == -1)
+            return -1;
+        int rightHeight = dfsHeight(root.right);
+        if(rightHeight == -1)
+            return -1;
+        
+        if(Math.abs(leftHeight - rightHeight) > 1)
+            return -1;
+        return 1 + Math.max(leftHeight, rightHeight);
     }
+
     public static void main(String[] args) {
         Node root = new Node(1);
         root.left = new Node(2);
