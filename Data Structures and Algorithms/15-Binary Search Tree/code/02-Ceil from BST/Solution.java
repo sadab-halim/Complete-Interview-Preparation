f6c3fa3ddec8b4f9c1d2827eb6bd4878dde8cc6c
Sadab Halim <sadabhalim7@gmail.com>
2022-04-08 16:50:09 +0530

    BST

---
+++ Data Structures and Algorithms/15-Binary Search Tree/code/02-Ceil from BST/Solution.java
@@ -0,0 +1,19 @@
+class Solution {
+    public static int findCeil(TreeNode root, int key) {
+        int ceil = -1;
+        while(root != null) {
+            if(root.data == key) {
+                ceil = root.data;
+                return ceil;
+            }
+            if(key > root.data) {
+                root = root.right;
+            }
+            else {
+                ceil = root.data;
+                root = root.left;
+            }
+        }
+        return ceil;
+    }
+}
