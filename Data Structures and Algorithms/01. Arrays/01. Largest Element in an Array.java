d09d186939d559331ef458b030dccadc1be117d8
Sadab Halim <sadabhalim7@gmail.com>
2022-04-25 23:00:08 +0530

    initial commit

--- Data Structures and Algorithms/01. Arrays/01. Largest Element in an Array.java
+++ Data Structures and Algorithms/01. Arrays/01. Largest Element in an Array.java
@@ -1,3 +1,20 @@
-public class 01. Largest Element in an Array {
-    
+class Main {
+    public static int getLargest(int[] arr) {
+        int size = arr.length;
+        for(int i = 0; i < size; i++) {
+            for(int j = i + 1; j < size; j++) {
+                if(arr[i] > arr[j]) {
+                    int temp = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = temp;
+                }
+            }
+        }
+        return arr[total - 1];
+    }
+
+    public static void main(String[] args) {
+        int arr[] = {10, 20, 50, 30, 40, 70};
+        System.out.println(getLargest(arr));
+    }
 }
