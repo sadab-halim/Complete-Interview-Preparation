bd9bb6b715358fba685a5626c9ad7b82d2bbd051
Sadab Halim <sadabhalim7@gmail.com>
2022-04-25 23:21:31 +0530

    initinal commit

--- Data Structures and Algorithms/01. Arrays/04. Reverse an Array.java
+++ Data Structures and Algorithms/01. Arrays/04. Reverse an Array.java
@@ -1,3 +1,20 @@
-public class 01. Reverse an array {
-    
+class Main {
+    public static void reverse(int arr[]) {
+        int size = arr.length;
+        int low = 0;
+        int high = size - 1;
+        
+        while(low < high) {
+            int temp = arr[low];
+            arr[low] = arr[high];
+            arr[high] = temp;
+            low++;
+            high--;
+        }
+    }
+
+    public static void main(String[] args) {
+        int arr[] = {10, 20, 30, 40, 50};
+        System.out.println(reverse(arr););
+    }
 }
