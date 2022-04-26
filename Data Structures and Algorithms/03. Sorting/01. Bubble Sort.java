fba9bb56bda568e8cda49249b78c324d221fa825
Sadab Halim <sadabhalim7@gmail.com>
2022-04-26 20:35:27 +0530

    updated

--- Data Structures and Algorithms/03. Sorting/01. Bubble Sort.java
+++ Data Structures and Algorithms/03. Sorting/01. Bubble Sort.java
@@ -1,3 +1,40 @@
-public class 01. Bubble Sort {
-    
+/* --------------- NAIVE METHOD ----------------- */
+class Main {
+    // function of bubble sort
+    public static void bubbleSort(int arr[]) {
+        int size = arr.length;
+
+        for(int i = 0; i < size - 1; i++) {
+            for(int j = 0; j < size - (i - 1); j++) {
+                if(arr[j] > arr[j + 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+    }
+}
+
+/* --------------- EFFICIENT METHOD ----------------- */
+class Main {
+    // function of bubble sort
+    public static void bubbleSort(int arr[]) {
+        int size = arr.length;
+
+        for(int = 0; i < (size - 1); i++) {
+            boolean swapped = false;
+            for(int j = 0; j < (size - i - 1); j++) {
+                if(arr[j] > arr[j + 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+
+                    swapped = true;
+                }
+            }
+            if(!swapped)
+                break;
+        }
+    }
 }
--- Data Structures and Algorithms/03. Sorting/03. Insertion Sort.java
+++ Data Structures and Algorithms/03. Sorting/03. Insertion Sort.java
@@ -1,3 +1,13 @@
-public class 03. Insertion Sort {
-    
+public static void insertionSort(int arr[]) {
+    int n = arr.length;
+    for(int i = 1; i< n; i++) {
+        int currentElement = arr[i];
+        int j = i - 1;
+
+        while(j >= 0 && arr[j] > currentElement) {
+            arr[j + 1] = arr[j];
+            j = j - 1;
+        }
+        arr[j + 1] = currentElement;
+    }
 }
