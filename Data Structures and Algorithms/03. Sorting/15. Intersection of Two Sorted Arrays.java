033736802ccff10244355cca8458ce6528db5dc9
Sadab Halim <sadabhalim7@gmail.com>
2022-04-28 18:49:37 +0530

    initial commit

--- Data Structures and Algorithms/03. Sorting/15. Intersection of Two Sorted Arrays.java
+++ Data Structures and Algorithms/03. Sorting/15. Intersection of Two Sorted Arrays.java
@@ -1,3 +1,42 @@
-public class 15. Intersection of Two Sorted Arrays {
-    
+/* Find the intersection of two sorted arrays. Given 2 sorted arrays
+    find all the elements which occur in both the ways
+*/
+
+/*
+Example:
+Input: A: [1 2 3 3 4 5 6], B: [3 3 5]
+Output: [3 3 5]
+*/
+
+/* ---------- APPROACH ---------- 
+1. Scan both arrays simultaneously
+2. Compare element of both the arrays
+3. If both of the values are the same, print it
+4. Else, increment the counter of the array with smaller element
+*/
+
+class Main {
+    public static void intersection(int A[], int B[]) {
+        int i = 0, j = 0;
+
+        while(i < A.length && j < B.length) {
+            if(A[i] < B[j]) {
+                i++;
+            } else if (A[i] > B[j]) {
+                j++;
+            } else {
+                System.out.print(A[i] + " ");
+                i++;
+                j++;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        int A[] = {1, 2, 3, 4, 5, 6};
+        int B[] = {4, 9, 13, 15, 16, 17};
+
+        intersection(A, B);
+    }
 }
+
--- Data Structures and Algorithms/03. Sorting/16. Union of Two Sorted Arrays.java
+++ Data Structures and Algorithms/03. Sorting/16. Union of Two Sorted Arrays.java
@@ -1,3 +1,53 @@
-public class 16. Union of Two Sorted Arrays {
-    
+/* Find the union of two sorted arrays. Given 2 sorted arrays
+    print elements of both the arrays
+*/
+
+/*
+Example:
+Input: A: [1 2 3 3 4 5 6], B: [3 3 5]
+Output: [1 2 3 3 3 3 4 5 5 6] 
+*/
+
+/* -------------- APPROACH -------------- 
+1. Scan both arrays simultaneously
+2. Compare element of both the array and print the smaller value
+3. Increment the counter of the array with the smaller value
+4. After smaller array is completed. Print out the remaining elements of the larger array.
+*/
+
+class Main {
+    public static void union(int A[], int B[]) {
+        int i - 0, j = 0;
+
+        while(i < A.length && j < B.length) {
+            if(A[i] < B[j]) {
+                System.out.print(A[i] + " ");
+                i++;
+            } else if (A[i] > B[j]) {
+                System.out.print(B[j] + " ");
+                j++;
+            } else {
+                System.out.print(A[i] + " ");
+                i++;
+                j++;
+            }
+        }
+
+        while(i < A.length) {
+            System.out.print(A[i] + " ");
+            i++;
+        }
+
+        while(j < B.length) {
+            System.out.print(B[j] + " ");
+            j++;
+        }
+    }
+
+    public static void main(String[] args) {
+        int A[] = {1, 2, 3, 4, 5, 6};
+        int B[] = {4, 9, 13, 15, 16, 17};
+        
+        union(A, B);
+    }
 }
