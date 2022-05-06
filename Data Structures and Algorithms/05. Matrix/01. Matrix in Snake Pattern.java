1f735b9947ebc07f8517d46661c4a9bbe2c90793
Sadab Halim <sadabhalim7@gmail.com>
2022-05-06 09:04:45 +0530

    intial commit

--- Data Structures and Algorithms/05. Matrix/01. Matrix in Snake Pattern.java
+++ Data Structures and Algorithms/05. Matrix/01. Matrix in Snake Pattern.java
@@ -1,3 +1,51 @@
-public class 01. Matrix in Snake Pattern {
-    
+/* ----------- Matrix in a Snake Pattern ----------- */
+
+/* Given a n x n matrix, you have to print the elements of the
+   matrix in the snake pattern.
+*/
+
+/*
+Example:
+Input: mat[][] = {{10, 20, 30, 40},
+                  {15, 25, 35, 45},
+                  {27, 29, 37, 48},
+                  {32, 33, 39, 50}};
+Output: 10 20 30 40 45 35 25 15 27 29 37 48 50 39 33 32
+
+Input: mat[][] = {{1, 2, 3},
+                  {4, 5, 6},
+                  {7, 8, 9}};
+Output: 1 2 3 6 5 4 7 8 9
+*/
+
+/* ---- APPROACH ----
+ We traverse all rows. For every row, we check if it is even or odd. If
+even, we print from left to right else print from right to left.
+*/
+
+import java.util.*;
+class Main {
+    public static spiralMatrix(int mat[][]) {
+        for(int i = 0; i < mat.length; i++) {
+            if(i % 2 == 0) {
+                for(int j = 0; j < mat[0].length; j++)
+                    System.out.print(mat[i][j] + " ");
+            } else {
+                for(int j = mat[0].length - 1; j >= 0; j--)
+                    System.out.print(mat[i][j] + " ");
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        int mat[][] = new int[][]
+        {
+            { 10, 20, 30, 40 },
+            { 15, 25, 35, 45 },
+            { 27, 29, 37, 48 },
+            { 32, 33, 39, 50 }
+        };
+ 
+        spiralMatrix(mat);
+    }
 }
--- Data Structures and Algorithms/05. Matrix/02. Matrix Boundary Traversal.java
+++ Data Structures and Algorithms/05. Matrix/02. Matrix Boundary Traversal.java
@@ -1,3 +1,81 @@
-public class 02. Matrix Boundary Traversal {
-    
+/* --------------- PRINT BOUNDARY ELEMENTS OF A MATRIX --------------- */
+
+/* 
+Given a matrix of size n x m. Print the boundary elements of the matrix. 
+Boundary elements are those elements which are not surrounded by elements on all four directions, 
+i.e. elements in first row, first column, last row and last column.
+*/
+
+/*
+Example-1:
+
+Input :
+        1 2 3 4  
+        5 6 7 8
+        1 2 3 4
+        5 6 7 8
+
+Output : 
+         1 2 3 4 
+         5     8 
+         1     4 
+         5 6 7 8
+
+Explanation:The boundary elements of the
+matrix is printed.
+
+Example-2:
+
+Input:
+        1 2 3   
+        5 6 7 
+        1 2 3 
+
+Output: 
+        1 2 3   
+        5   7 
+        1 2 3 
+
+Explanation:The boundary elements of the 
+matrix is printed.
+*/
+
+/* --------- APPROACH --------- */
+/*
+1. Traverse the array from start to end.
+2. Assign the outer loop to point the row and the inner row to traverse the elements of row.
+3. If the element lies in the boundary of matrix, then print the element, i.e. if the element lies in 1st row, 1st column, last row, last column
+4. If the element is not boundary element print a blank space.
+*/
+
+/* ------------ T.C: O(n), A.S: O(1) ------------ */
+class Main {
+    static int R = 4, C= 4;
+
+    public static void printBoundary(int arr[][]) {
+        if(R == 1){
+            for(int i = 0; i < C; i++)
+                System.out.print(mat[0][i] + " ");
+        } else if(C == 1) {
+            for(int i = 0; i < R; i++)
+                System.out.print(mat[i][0] + " ");
+        } else {
+            for(int i = 0; i < C; i++)
+                System.out.print(mat[0][i] + " ");
+            for(int i = 1; i < R; i++)
+                System.out.print(mat[i][C - 1] + " ");
+            for(int i = C - 2; i >= 0; i--)
+                System.out.print(mat[R - 1][i] + " ");
+            for(int i = R - 2; i >= 1; i--)
+                System.out.print(mat[i][0] + " ");
+        }
+    }
+    public static void main(String[] args) {
+        int arr[][] = {{1, 2, 3, 4},
+                       {5, 6, 7, 8},
+                       {9, 10, 11, 12},
+                       {13, 14, 15, 16}};
+        
+        printBoundary(arr);
+    }
 }
