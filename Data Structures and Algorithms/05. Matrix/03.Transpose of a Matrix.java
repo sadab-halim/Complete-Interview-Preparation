8220c2fa079da899b912fe4e497162e7af0b97e1
Sadab Halim <sadabhalim7@gmail.com>
2022-05-08 18:02:55 +0530

    initial commit

--- Data Structures and Algorithms/05. Matrix/03.Transpose of a Matrix.java
+++ Data Structures and Algorithms/05. Matrix/03.Transpose of a Matrix.java
@@ -1,3 +1,60 @@
-public class 03.Transpose of a Matrix {
+/* -------------- TRANSPOSE OF A MATRIX -------------- */
+
+/* -------------- NAIVE METHOD, T.C: O(n^2), A.S: O(n) --------------*/
+class Main {
+    static int n = 4;
     
+    public static void transpose(int mat[][]) {
+        int temp[][] = new int[n][n];
+
+        for(int i = 0; i < n; i++)
+            for(int j = 0; j < n; j++)
+                temp[i][j] = mat[j][i];
+        
+        for(int i = 0; i < n; i++)
+            for(int j = 0; j < n; j++)
+                mat[i][j] = temp[i][j];
+    }
+
+    public static void main(String[] args) {
+        int arr[][] = {{1, 2, 3, 4},
+                       {5, 6, 7, 8},
+                       {9, 10, 11, 12},
+                       {13, 14, 15, 16}};
+        transpose(arr);
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < n; j++)
+                System.out.print(arr[i][j] + " ");
+            System.out.println();
+        }
+    }
+}
+
+/* -------------- EFFICIENT METHOD, T.C: O(n), A.S: O(n) --------------*/
+class Main {
+    static int n = 4;
+
+    public static void swap(int arr[][], int i, int j) {
+        int temp = mat[i][j];
+        mat[i][j] = mat[j][i];
+        mat[j][i] = temp;
+    }
+
+    public static void transpose(int arr[][]) {
+        for(int i = 0; i < n; i++)
+            for(int j = i + 1; j < n; j++)
+                swap(arr, i, j);
+    }
+    public static void main(String[] args) {
+        int arr[][] = {{1, 2, 3, 4},
+                       {5, 6, 7, 8},
+                       {9, 10, 11, 12},
+                       {13, 14, 15, 16}};
+        transpose(arr);
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < n; j++)
+                System.out.print(arr[i][j] + " ");
+            System.out.println();
+        }
+    }
 }
--- Data Structures and Algorithms/05. Matrix/04.Rotate Matrix Anticlockwise by 90.java
+++ Data Structures and Algorithms/05. Matrix/04.Rotate Matrix Anticlockwise by 90.java
@@ -1,3 +1,27 @@
-public class 04.Rotate Matrix Anticlockwise by 90 {
-    
+class Main {
+    static int n = 4;
+    public static void rotate90(int arr[][]) {
+        int temp[][] = new int[n][n];
+
+        for(int i = 0; i < n; i++)
+            for(int j = 0; j < n; j++)
+                temp[n - j - 1][i] = arr[i][j];
+        
+        for(int i = 0; i < n; i++)
+            for(int j = 0; j < n; j++)
+                arr[i][j] = temp[i][j];
+    }
+
+    public static void main(String[] args) {
+        int arr[][] = {{1, 2, 3, 4},
+                       {5, 6, 7, 8},
+                       {9, 10, 11, 12},
+                       {13, 14, 15, 16}};
+        rotate90(arr);
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < n; j++)
+                System.out.print(arr[i][j] + " ");
+            System.out.println();
+        }   
+    }
 }
