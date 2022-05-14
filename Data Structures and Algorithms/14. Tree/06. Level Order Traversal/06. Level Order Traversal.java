cd04c4d02490f55b7d4afa802cc2919a6c72244a
Sadab Halim <sadabhalim7@gmail.com>
2022-05-14 20:45:05 +0530

    initial commit

--- Data Structures and Algorithms/14. Tree/06. Level Order Traversal/06. Level Order Traversal.java
+++ Data Structures and Algorithms/14. Tree/06. Level Order Traversal/06. Level Order Traversal.java
@@ -1,3 +1,22 @@
-public class 06. Level Order Traversal {
-    
+class Main {
+    public static List<Integer> levelOrder(Node root) {
+        Queue<Node> queue = new LinkedList<Node>();
+        List<Integer> wrapList = new List<Integer>();
+
+        if(root == null)
+            return wrapList;
+        
+        queue.offer(root);
+        while(!queue.isEmpty()) {
+            if(queue.peek().left != null)
+                queue.offer(queue.peek().left);
+            if(queue.peek().right != null)
+                queue.offer(queue.peek(). right);
+            wrapList.add(queue.poll().val);
+        }
+        return wrapList;
+    }
+    public static void main(String[] args) {
+        
+    }
 }
