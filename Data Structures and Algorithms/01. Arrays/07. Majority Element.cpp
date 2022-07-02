38ffe2d501003ef52353386642c8f2695acaa3f1
Sadab Halim <sadabhalim7@gmail.com>
2022-07-02 22:42:49 +0530

    initial commit

---
+++ Data Structures and Algorithms/01. Arrays/07. Majority Element.cpp
@@ -0,0 +1,31 @@
+/* ------- MAJORITY ELEMENT ------- */
+/*
+Given an array nums of size n, return the majority element.
+The majority element is the element that appears more than ⌊n / 2⌋ times. You may assume that the majority element always exists in the array.
+
+Example-1:
+Input: nums = [3,2,3]
+Output: 3
+
+Example-2:
+Input: nums = [2,2,1,1,1,2,2]
+Output: 2
+*/
+
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        int count = 0;
+        int elem = 0;
+        
+        for(int num: nums){
+            if(count == 0)
+                elem = num;
+            if(num == elem) 
+                count += 1;
+            else
+                count -=1;
+        }
+        return elem;
+    }
+};
