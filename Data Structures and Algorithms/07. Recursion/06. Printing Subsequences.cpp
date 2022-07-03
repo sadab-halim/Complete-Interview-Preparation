61ad00fa3c353fbbf0fe7334f845b4482f2aedb6
Sadab Halim <sadabhalim7@gmail.com>
2022-07-03 22:51:41 +0530

    initial commit

---
+++ Data Structures and Algorithms/07. Recursion/06. Printing Subsequences.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void print_subsequences(int index, vector<int> &ds, int arr[], int n){
+    if(index == n){
+        
+    }
+}
+
+int main(){
+    int arr[] = {3, 1, 2};
+    int n = 3;
+    vector<int> ds;
+    print_subsequences(0, ds, arr, n);
+    return 0;
+}
