0b5c027004e1f485d469c9a4fc7df50c1bab1061
Sadab Halim <sadabhalim7@gmail.com>
2022-07-05 13:12:57 +0530

    initial commit

---
+++ Data Structures and Algorithms/14. Tree/01. Representation of Tree.cpp
@@ -0,0 +1,16 @@
+struct Node{ 
+  int key; 
+  struct Node *left; 
+  struct Node *right; 
+  Node(int k){
+      key=k;
+      left=right=NULL;
+  }
+};
+    
+int main(){
+	Node *root=new Node(10);
+	root->left=new Node(20);
+	root->right=new Node(30);
+	root->left->left=new Node(40);
+}
