271903e0c38b5bd786dc4a22677cea3bd51e742c
Sadab Halim <sadabhalim7@gmail.com>
2022-07-07 00:33:58 +0530

    initial commit

---
+++ Data Structures and Algorithms/10. Linked List/14. Reverse a SLL.cpp
@@ -0,0 +1,10 @@
+ListNode* reverse_list(ListNode* head){
+    ListNode *new_head = NULL;
+    while(head != null){
+        ListNode *next = head -> next;
+        head->next = new_head;
+        new_head = head;
+        head = next;
+    }
+    return new_head;
+}
