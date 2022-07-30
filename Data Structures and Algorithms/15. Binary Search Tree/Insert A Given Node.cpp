6e01b0f3872f1f16b96470f098e5c3c0792585af
Sadab Halim <sadabhalim7@gmail.com>
2022-07-30 12:54:27 +0530

    updated

---
+++ Data Structures and Algorithms/15. Binary Search Tree/Insert A Given Node.cpp
@@ -0,0 +1,26 @@
+class Solution{
+public:
+    TreeNode *insertIntoBST(TreeNode* root, int val){
+        if(root == NULL) return new TreeNode(val);
+
+        TreeNode *curr = root;
+        while(true){
+            if(val >= curr->val){
+                if(curr->right != NULL)
+                    curr = curr->right;
+                else{
+                    curr->right = new TreeNode(val);
+                    break;
+                }
+            } else {
+                if(curr->left != NULL)
+                    curr = curr->left;
+                else{
+                    curr->left = new TreeNode(val);
+                    break;
+                }
+            }
+        }
+        return root;
+    }
+};
